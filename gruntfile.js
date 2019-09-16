f2708d368888c11f812f62c5374303dcf40659ac
Tomasz Pankowski <pankowski_tomasz@o2.pl>
2019-09-16 22:27:21 +0200

    project configuration

---
+++ gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        concat:{
+            js:{
+                src:[
+                    "js/jquery-3.4.1.min.js",
+                    "js/jquery.lazy.min.js",
+                    "js/bootstrap.bundle.min.js",
+                    "js/main.js"
+                ],
+                dest:"js/main.concat.js"
+            },
+            css:{
+                src:[
+                    "css/bootstrap.min.css",
+                    "css/styles.css"
+                ],
+                dest:"js/styles.concat.css"
+            }
+        },
+        uglify:{
+            compress:{
+                files:{
+                    "js/main.min.js":"<%= concat.js.dest %>"
+                }
+            }
+        },
+        cssmin:{
+            compress:{
+                files:{
+                    "css/styles.min.css":"<%= concat.css.dest %>"
+                }
+            }
+        },
+        copy:{
+            pub:{
+                files:[
+                    {src:"./*.html",dest:"pub/"},
+                    {src:"./*.php",dest:"pub/"},
+                    {src:"css/styles.min.css",dest:"pub/"},
+                    {src:"js/main.min.js",dest:"pub/"}
+                ]
+            }
+        }
+    });
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.registerTask("default",["concat","uglify","cssmin"]);
+    //grunt.registerTask("default",["concat","uglify","cssmin","copy"]);
+};
